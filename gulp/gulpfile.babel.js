01f78a77cc8e1e63a44b1e04c602e5ca30dfd003
hisland <hisland@qq.com>
2015-12-23 12:43:26 +0800

    gulp babel

---
+++ gulp/gulpfile.babel.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import gulp from 'gulp';
+
+gulp.task('default', function(){
+  console.log('我是基于babel的, 可以用 es6/es7');
+  console.log('需要配置 .babelrc');
+  console.log('需要配置 在当前目录下 npm install babel-preset-es2015');
+});
+
