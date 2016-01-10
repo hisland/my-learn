6485a1feeeda9b33e580126bb88b573d45b289fd
hisland <hisland@qq.com>
2016-01-10 20:48:46 +0800

    01/10/16 20:48:46 normal commit

--- gulp/glob-stream/test.js
+++ gulp/glob-stream/test.js
@@ -4,6 +4,8 @@ var map = require('map-stream');
 
 var globStream = gs.create(['../through2/*.js', './*.js']);
 
+console.log('通过 ordered-read-streams 保证 glob 的匹配是 从左到右,从上到下 pipe 的');
+
 var count = 0;
 globStream.pipe(map(function(file, cb) {
   console.log(count++, file);
--- jquery-payload.js
+++ jquery-payload.js
@@ -1,16 +1,17 @@
 $.ajax({
-  url: '/cms/index/addcontent',
+  url: 'http://test.com:3333/index/update_key',
   type: 'POST', // 必须是POST
   contentType: 'application/json; charset=UTF-8',
-  data: JSON.stringify({
-    channel: cc,
-    content: v.content.trim(),
-    title: v.title.trim(),
-    type: "text"
-  }), // 数据 必须是字符串 会原样送到服务器
+  data: JSON.stringify({"id":"9","data":{"URL":"/student/list/**abc"}}), // 数据 必须是字符串 会原样送到服务器
   dataType: 'text', // 期望返回类型 json, xml
   processData: false, // 防止 data 被预处理
   success: function(data) {
-
+    console.log(data);
   }
 });
+
+
+// 记得服务器 返回这些头
+// this.header('Access-Control-Allow-Origin', '*');
+// this.header('Access-Control-Allow-Headers', 'Content-Type'); // 不可以用 *
+
