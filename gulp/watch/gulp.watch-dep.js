2ced66edf05096e45b72255d6ccb5347b4dcdb9c
hisland <hisland@qq.com>
2016-01-24 21:44:11 +0800

    01/24/16 21:44:11 normal commit

--- gulp/watch/gulp.watch-dep.js
+++ gulp/watch/gulp.watch-dep.js
@@ -5,7 +5,7 @@ gulp.task('default', function() {
   gulp.watch(['gulp.watch2.js', 'gulp.watch.js'], ['dep']);
 });
 
-gulp.task('dep', function() {
+gulp.task('dep', function() { // 这里要带回调必须在内部回调, 否则会一直挂起, 后续的watch一起不执行
 
   console.log(3);
 });
