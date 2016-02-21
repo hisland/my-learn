75f9638fc2aff36b13742b74922ad6162e97620f
hisland <hisland@qq.com>
2016-02-21 16:58:01 +0800

    02/21/16 16:58:01 normal commit

--- jquery-payload.js
+++ jquery-payload.js
@@ -2,7 +2,12 @@ $.ajax({
   url: 'http://test.com:3333/index/update_key',
   type: 'POST', // 必须是POST
   contentType: 'application/json; charset=UTF-8',
-  data: JSON.stringify({"id":"9","data":{"URL":"/student/list/**abc"}}), // 数据 必须是字符串 会原样送到服务器
+  data: JSON.stringify({
+    "id": "9",
+    "data": {
+      "URL": "/student/list/**abc"
+    }
+  }), // 数据 必须是字符串 会原样送到服务器
   dataType: 'text', // 期望返回类型 json, xml
   processData: false, // 防止 data 被预处理
   success: function(data) {
@@ -11,7 +16,24 @@ $.ajax({
 });
 
 
-// 记得服务器 返回这些头
+$.ajax({
+  url: '/role/roleAuthorize/add',
+  type: 'POST',
+  contentType: 'application/json; charset=UTF-8',
+  data: JSON.stringify({
+    roleId: item.roleInfo.id,
+    userIds: rs
+  }),
+  dataType: 'json',
+  processData: false,
+  success: function(rs) {
+    if (!rs.code) {
+      $state.reload();
+    }
+  }
+});
+
+
+// 跨域的时候, 记得服务器 返回这些头
 // this.header('Access-Control-Allow-Origin', '*');
 // this.header('Access-Control-Allow-Headers', 'Content-Type'); // 不可以用 *
-
