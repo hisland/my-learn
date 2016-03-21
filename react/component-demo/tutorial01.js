968d01a831ff7f1e8f5abd561820c08106dd848e
hisland <hisland@qq.com>
2016-03-21 23:01:33 +0800

    03/21/16 23:01:33 normal commit

---
+++ react/component-demo/tutorial01.js
@@ -0,0 +1,13 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+);
