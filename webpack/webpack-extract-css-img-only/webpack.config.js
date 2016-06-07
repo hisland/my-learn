b482d1265f949f0541c07207cad94df18653e5c2
hisland <hisland@qq.com>
2016-06-07 11:01:59 +0800

    06/07/16 11:01:59 normal commit

---
+++ webpack/webpack-extract-css-img-only/webpack.config.js
@@ -0,0 +1,41 @@
+var webpack = require("webpack");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// var extractCSS = new ExtractTextPlugin('[name].css');
+// var extractLESS = new ExtractTextPlugin('[name].less');
+// console.log(ExtractTextPlugin.extract('style-loader', 'css-loader'));
+module.exports = {
+  entry: './css.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      // { test: /\.css/i, loader: ExtractTextPlugin.extract('css') },
+      { test: /\.less/i, loader: ExtractTextPlugin.extract('css!less') },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+        }
+      },
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("main.css"),
+
+    // extractCSS,
+    // extractLESS,
+  ]
+}
