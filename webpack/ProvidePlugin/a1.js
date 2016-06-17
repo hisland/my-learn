9b63d2ce8377e386ce0da9ce4a92d579860d286e
hisland <hisland@qq.com>
2016-06-17 15:10:16 +0800

    webpack ProvidePlugin

---
+++ webpack/ProvidePlugin/a1.js
@@ -0,0 +1,1 @@
+console.log($('#id')); // 我在用$, 会自动给我打包jquery
---
+++ webpack/ProvidePlugin/webpack.config.js
@@ -0,0 +1,24 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    bundle1: './a1',
+  },
+  output: {
+    filename: '[name].js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery'
+    }),
+  ],
+}
+
+/*
+
+官网说是自动注入模块, 这样就不用显示地 require('XXX') 了
+可以直接使用 $ 这些 key 作为全局 对象使用 (free variable)
+
+一直以为是表示此变量已经全局定义了...比如script引入的..
+
+*/
