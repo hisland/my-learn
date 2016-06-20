c4afd3c07e040c890fa0bae20398307c8e0a460a
hisland <hisland@qq.com>
2016-06-20 09:49:03 +0800

    06/20/16 09:49:03 normal commit

--- webpack/html-webpack-plugin/basic-with-template-jsp/webpack.config.js
+++ webpack/html-webpack-plugin/basic-with-template-jsp/webpack.config.js
@@ -20,7 +20,8 @@ var webpackConfig = {
     new HtmlWebpackPlugin({
       title: 'aha',
       // template: './say.html', // 普通html, 直接写上
-      template: 'raw!./say.jsp', // 如果是jsp, 用raw-loader
+      // template: 'raw!./say.jsp', // 如果是jsp, 用raw-loader
+      template: '!!raw!./say.jsp', // raw-loader, 阻止经过其它所有的loader, 这个坑也是费了不少时间才发现的
       filename: '../say2.jsp'
     })
   ]
