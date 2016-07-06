40f58b45778534e76cb485ff53adaef1440475d1
hisland <hisland@qq.com>
2016-07-06 10:07:10 +0800

    update DefinePlugin test

--- webpack/DefinePlugin/a1.js
+++ webpack/DefinePlugin/a1.js
@@ -1,3 +1,7 @@
 exports.v = VERSION;
 
 console.log(TWO);
+
+if (SOME) {
+  console.log(444);
+}
--- webpack/DefinePlugin/webpack.config.js
+++ webpack/DefinePlugin/webpack.config.js
@@ -10,7 +10,23 @@ module.exports = {
   plugins: [
     new webpack.DefinePlugin({
       TWO: '1+1',
-      VERSION: '3.14'
+      VERSION: '3.14',
+      SOME: false
     }),
+    new webpack.optimize.UglifyJsPlugin({
+      beautify: true,
+      compress:{
+        drop_debugger: true,
+        conditionals: true,
+        drop_console: true,
+      }
+    })
   ],
 }
+
+
+/*
+
+定义 free variable, 相当于全局变量, 随处可用
+
+*/
