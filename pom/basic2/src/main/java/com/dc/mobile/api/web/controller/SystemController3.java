ff21abc78d0c9ed70f80cdb95e5bd141bc510246
hisland <hisland@qq.com>
2017-02-21 11:17:35 +0800

    add maven-pom test

---
+++ pom/basic2/src/main/java/com/dc/mobile/api/web/controller/SystemController3.java
@@ -0,0 +1,11 @@
+package com.dc.mobile.api.web.controller;
+
+
+import com.dc.mobile.api.web.controller.;
+
+public class SystemController3 {
+
+    public static void main() {
+        SystemController a = new SystemController();
+    }
+}
