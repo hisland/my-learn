27866728df6af6b57c5fbeaf95f09c8e3e8a4381
hisland <hisland@qq.com>
2017-06-01 09:17:32 +0800

    update to "postcss-salad": "^2.0.1"

--- postcss/08.postcss-bem/processor.js
+++ postcss/08.postcss-bem/processor.js
@@ -23,6 +23,8 @@ const salad = require('postcss-salad')
     .process(fs.readFileSync('./main.css') )
     .then(function(rs) {
       console.log('使用了 postcss-bem, 有2个出不来, 不知道为什么')
+      console.log('靠, 版本问题, postcss 6.x 不行, postcss 5.x可以')
+      console.log('更新到 postcss-salad@2.0.1, postcss 6.x 可以了')
       console.log(rs.css)
     })
 })()
