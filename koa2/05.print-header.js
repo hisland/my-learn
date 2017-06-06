c851e13df7f5d85d6626695af4e1e14182b4a7f3
hisland <hisland@qq.com>
2017-06-06 15:10:14 +0800

    some change

---
+++ koa2/05.print-header.js
@@ -0,0 +1,15 @@
+// 需要使用 node v7.6.0 or higher for ES2015 and async function support
+
+const Koa = require('koa')
+const app = new Koa()
+const port = 9002
+
+app.use(function(ctx) {
+  console.log(ctx.request.url)
+  console.log(ctx.request.header)
+  ctx.body = 'any path will get this: hello!'
+})
+
+app.listen(port)
+console.log(`listen on: ${port}`)
+
