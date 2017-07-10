89d009bb5d39a7aa7a15e8f196fa7375edc27f5d
hisland <hisland@qq.com>
2017-07-10 18:51:18 +0800

    update eslint demos

--- eslint/05.multi-line-expr-not-treat-as-1line/some.js
+++ eslint/05.multi-line-expr-not-treat-as-1line/some.js
@@ -1,15 +1,9 @@
 // 多行表达式, eslint 并不会认为一行, 按常规理解(一行就是一行)执行
 
-// eslint-disable-next-line no-undef
-console.log(
-
-  some(3), // eslint-disable-line no-console
-
-  4);
-
 // eslint-disable-next-line no-console
 console.log(
 
-  some(3), // eslint-disable-line no-undef
+  some(3),
+  console.log('qq'), // 这行报错
 
   4);
