46d2932deafb6e9bb0ebbdb46d609f41c6bf2b35
hisland <hisland@qq.com>
2017-08-11 10:48:11 +0800

    add some py3 test

---
+++ python3/tutorial/06.if-elif-else.py
@@ -0,0 +1,13 @@
+xx = 3
+
+if xx < 5 :
+  print('halo')
+else :
+  print('bbq')
+
+if xx < 3:
+  print('halo')
+elif xx == 3:
+  print('elif bbq')
+else:
+  print('bbq')
--- python3/tutorial/temp.py
+++ python3/tutorial/temp.py
@@ -1,21 +1,2 @@
-num1 = [22, 33, 44, 55]
-print(num1)
-print(num1[0])
-print(num1[-1])
-
-num2 = num1[2:4]
-print(num2)
-
-num3 = num1 + [4, 5, 6]
-print(num3)
-
-num1[0] = 1
-print(num1)
-
-num1.append(99)
-print(num1)
-
-num1[1:3] = [3]
-print(num1)
-
-print(len(num1))
+aa = 3
+print(aa)
