0275ee59998a73f39e9025c97056aa19f65ef242
hisland <hisland@qq.com>
2017-10-26 12:18:44 +0800

    update mongoose test

--- mongoose/12.useMongoClient-new-connection-logic.js
+++ mongoose/12.useMongoClient-new-connection-logic.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
+
+// http://mongoosejs.com/docs/api.html#index_Mongoose-connect
+// [options.useMongoClient] <Boolean> false by default, set to true to use new mongoose connection logic
 mongoose.connect('mongodb://localhost/test', { useMongoClient: true })
 
-let ss = mongoose.Schema({}, {strict: false}) // 不强制doc结构
-let Dog = mongoose.model('dog', ss, 'dog') // 第3个参数指定 collection 名字, 不会自动生成第一个参数的复数
+let ss = mongoose.Schema({}, {strict: false})
+let Dog = mongoose.model('dog', ss, 'dog')
 
 let dd = new Dog({ prop1: 'hey', prop2: 'hey2',  })
 dd.save(function(err) {
