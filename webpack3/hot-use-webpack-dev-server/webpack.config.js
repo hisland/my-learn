a0d0bab02900fa77a38f00ee8db4a3a99ec0dda9
hisland <hisland@qq.com>
2017-11-21 14:44:35 +0800

    add hot-use-webpack-dev-server

---
+++ webpack3/hot-use-webpack-dev-server/webpack.config.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: './src/entry.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+  },
+  module: {
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(), // 只需要这里, webpack-dev-server 会自动处理其它细节
+  ],
+}
