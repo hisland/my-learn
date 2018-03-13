15bffc93979f73ed1f85096bd6b58e7874529a02
hisland <hisland@qq.com>
2018-03-13 11:37:42 +0800

    add webpack4 mode test

---
+++ webpack4/00.mode-development/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const webpack = require('webpack')
+
+module.exports = [
+  {
+    mode: 'development',
+    // mode: 'production',
+    entry: {
+      bundle: './entry',
+    },
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: '[name].js',
+      chunkFilename: 'chunk-[id].[name].js',
+    },
+    module: {
+      rules: [],
+    },
+    plugins: [],
+  },
+]
