0ff908f91fbd248cfeabaa01a18871c556125645
hisland <hisland@qq.com>
2018-04-22 01:29:16 +0800

    update gulp test

--- gulp3/gulpfile.js
+++ gulp3/gulpfile.js
@@ -1,14 +1,7 @@
-const gulp = require('gulp')
+var gulp = require('gulp');
 
-gulp.task('task1', ['task2'], function(done) {
-  setTimeout(function() {
-    done()
-  }, 100);
-})
-gulp.task('task2', function(done) {
-  setTimeout(function() {
-    done()
-  }, 30);
-})
+gulp.task('default', function(){
+  console.log('默认使用当前目录下的 gulpfile.js 文件');
+  console.log('可以使用 --gulpfile FILE 指定其它文件');
+});
 
-gulp.task('default', ['task1'])
