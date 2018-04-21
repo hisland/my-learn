309b466fae0af913bb557eab6d27711666d0785a
hisland <hisland@qq.com>
2018-04-22 01:31:19 +0800

    mongo 或者其它, 用 path 来修改obj局部, 而不是通过nest对象

---
+++ obj-key-path-think/key-path.js
@@ -0,0 +1,16 @@
+var aa = {
+  key1: 'value1',
+  key2: {
+    subKey2: 'value3'
+  }
+}
+
+var bb = {
+  key1: 'value1',
+  key1SubKey2: 'value3'
+}
+
+var cc = {
+  key1: 'value1',
+  'key1.subKey2': 'value3'
+}
