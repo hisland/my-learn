0daf1d344216332ff7db992b2696c34cbb495aa3
hisland <hisland@qq.com>
2018-06-29 18:16:22 +0800

    add some vuex test

---
+++ vuex/002-getters-other/main.js
@@ -0,0 +1,38 @@
+const { mapState, mapGetters, mapActions, mapMutations } = Vuex
+
+const store = new Vuex.Store({
+  strict: true,
+  state: {
+    count: 0,
+  },
+  mutations: {},
+  getters: {
+    what(state, getters) {
+      console.log(getters.other)
+      return state.count
+    },
+    other(state, getters) {
+      return 'other'
+    },
+  },
+  actions: {
+    actionDo(ctx, payload) {
+      console.log(33, ctx, payload)
+      setTimeout(function() {
+        console.log('do')
+      }, 500)
+    },
+  },
+})
+
+const vueApp = new Vue({
+  el: '#app',
+  store,
+  data: {},
+  computed: {
+    ...mapGetters(['what']),
+  },
+  methods: {
+    ...mapActions(['actionDo']),
+  },
+})
