f49f815cb7452227eaaa4604c44c01216443be1e
hisland <hisland@qq.com>
2018-07-22 13:21:18 +0800

    update ts test

--- typescript/00.basic-type/13.type-inference.ts
+++ typescript/00.basic-type/13.type-inference.ts
@@ -1,9 +1,9 @@
 // 类型可以自动推断
 
-let aa1= 'hey' // string
-let aa2= 3 // number
-let aa3= true // boolean
+let aa1 = 'hey' // string
+let aa2 = 3 // number
+let aa3 = true // boolean
 
-let aa4= [0, null] // (number|null)
+let aa4 = [0, null] // (number|null)
 
 let aa5 // any, (不能推断的, 默认是any的感觉?)
--- typescript/out.js
+++ typescript/out.js
@@ -1,10 +1,7 @@
-var aa1;
-(function (aa1) {
-    var bb = 3;
-    console.log(bb);
-    var aa2;
-    (function (aa2) {
-        var bb = 3;
-        console.log(bb);
-    })(aa2 = aa1.aa2 || (aa1.aa2 = {}));
-})(aa1 || (aa1 = {}));
+// 常数枚举只能使用常数枚举表达式并且不同于常规的枚举的是它们在编译阶段会被删除。
+// 常数枚举成员在使用的地方被内联进来。 这是因为常数枚举不可能有计算成员
+console.log(0 /* Up */);
+console.log(1 /* Down */);
+console.log(2 /* Left */);
+console.log(3 /* Right */);
+console.log(Directions); // 并没有这玩意儿
