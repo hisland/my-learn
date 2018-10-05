e18cf182f245af51370120d31f3e4b4a84bcaeda
hisland <hisland@qq.com>
2018-10-05 13:10:35 +0800

    update rust-wasm-test

--- rust-wasm-test/js/entry.js
+++ rust-wasm-test/js/entry.js
@@ -1,3 +1,46 @@
+const fps = new class {
+  constructor() {
+    this.fps = document.getElementById("fps");
+    this.frames = [];
+    this.lastFrameTimeStamp = performance.now();
+  }
+
+  render() {
+    // Convert the delta time since the last frame render into a measure
+    // of frames per second.
+    const now = performance.now();
+    const delta = now - this.lastFrameTimeStamp;
+    this.lastFrameTimeStamp = now;
+    const fps = 1 / delta * 1000;
+
+    // Save only the latest 100 timings.
+    this.frames.push(fps);
+    if (this.frames.length > 100) {
+      this.frames.shift();
+    }
+
+    // Find the max, min, and mean of our 100 latest timings.
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
+    for (let i = 0; i < this.frames.length; i++) {
+      sum += this.frames[i];
+      min = Math.min(this.frames[i], min);
+      max = Math.max(this.frames[i], max);
+    }
+    let mean = sum / this.frames.length;
+
+    // Render the statistics.
+    this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+  }
+};
+
 Promise.all([import('../pkg/game_bg'), import('../pkg/game.js')]).then(
   function([wasm, game]) {
     const { memory } = wasm
@@ -67,12 +110,15 @@ Promise.all([import('../pkg/game_bg'), import('../pkg/game.js')]).then(
 
     let animationId = null
     const renderLoop = () => {
+      fps.render()
+
       universe.tick()
 
       drawGrid()
       drawCells()
 
-      animationId = setTimeout(renderLoop, 100)
+      // animationId = requestAnimationFrame(renderLoop)
+      animationId = setTimeout(renderLoop, 50)
     }
     const isPaused = () => {
       return animationId === null
@@ -87,6 +133,7 @@ Promise.all([import('../pkg/game_bg'), import('../pkg/game.js')]).then(
 
     const pause = () => {
       playPauseButton.textContent = '▶'
+      // cancelAnimationFrame(animationId)
       clearTimeout(animationId)
       animationId = null
     }
