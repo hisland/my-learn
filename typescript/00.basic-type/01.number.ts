51acae10c88d5f11f50fdf6835e003cb5fa94a3c
hisland <hisland@qq.com>
2018-10-24 15:59:39 +0800

    update ts vars

--- typescript/00.basic-type/01.number.ts
+++ typescript/00.basic-type/01.number.ts
@@ -1,4 +1,4 @@
-let height: number = 6 // decimal
-let height: number = 0x6 // hex
-let height: number = 0b1010 // binary
-let height: number = 0o6 // octal
+let height0: number = 6 // decimal
+let height1: number = 0x6 // hex
+let height2: number = 0b1010 // binary
+let height3: number = 0o6 // octal
