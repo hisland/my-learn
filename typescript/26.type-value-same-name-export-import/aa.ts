222a1343428eae0e1d2a2ad094df9764ad513a89
hisland <hisland@qq.com>
2020-05-07 18:11:04 +0800

    add ts type-value-same-name-export-import

---
+++ typescript/26.type-value-same-name-export-import/aa.ts
@@ -0,0 +1,6 @@
+import { Hello } from './bb'
+
+const obj: Hello = { foo: 'abc', bar: '12' }
+
+console.log(obj)
+console.log(Hello)
---
+++ typescript/26.type-value-same-name-export-import/bb.ts
@@ -0,0 +1,9 @@
+export type Hello = {
+  foo: string
+  bar: string
+}
+
+export const Hello: Hello = {
+  foo: 'yes',
+  bar: 'good',
+}
