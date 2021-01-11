37ff462ff9570e040b0d44c42fb4d757408a11cd
hisland <hisland@qq.com>
2021-01-11 17:35:03 +0800

    add react-16 test

---
+++ react-16/84.multi-setSate-merged-and-render-once/main.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { foo: 1 }
+    console.log('App render')
+  }
+
+  setFoo = () => {
+    console.log('before: ', this.state.foo)
+    this.setState({ foo: this.state.foo + 1 })
+    console.log('after: ', this.state.foo)
+    this.setState({ bar: this.state.foo + 1 })
+    console.log('after: ', this.state.foo)
+
+    setTimeout(() => {
+      console.log('after 100ms: ', this.state)
+    }, 100)
+  }
+
+  render() {
+    console.log('run render every time')
+    const { foo } = this.state
+
+    return (
+      <div>
+        <ul>
+          <li>多次 setState 结果合并, 只触发一次 render</li>
+        </ul>
+
+        <div style={{ border: '1px solid pink', margin: '10px' }}>
+          <div>{foo}</div>
+          <button onClick={this.setFoo}>setFoo</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+if (import.meta.hot) {
+  import.meta.hot.accept()
+}
