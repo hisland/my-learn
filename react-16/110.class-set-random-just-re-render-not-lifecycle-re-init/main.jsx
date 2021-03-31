36f0243fc4a0d902c3ac09faef116a0c5351dcd4
hisland <hisland@qq.com>
2021-03-31 21:06:52 +0800

    add react-16 test

---
+++ react-16/110.class-set-random-just-re-render-not-lifecycle-re-init/main.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useRef } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { foo: 1 }
+    console.log('App constructor')
+  }
+
+  setAnyThing = () => {
+    this.setState({ random_to_trigger_refresh: Math.random() })
+  }
+
+  render() {
+    console.log('App render')
+    const { foo } = this.state
+
+    return (
+      <div>
+        <ul>
+          <li>
+            Sub1 setState, 并且触发了父组件的 prop 修改, 最终只 render 一次
+          </li>
+        </ul>
+
+        <div style={{ border: '1px solid pink', margin: '10px' }}>
+          <div>App: {foo}</div>
+          <button onClick={this.setAnyThing}>setAnyThing</button>
+          <Sub1 foo={this.state.foo}></Sub1>
+        </div>
+      </div>
+    )
+  }
+}
+class Sub1 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { myBar: 33 }
+    console.log('Sub1 constructor')
+  }
+
+  setAnyThing = () => {
+    this.setState({ random_to_trigger_refresh: Math.random() })
+  }
+
+  render() {
+    console.log('Sub1 render')
+    const { foo } = this.props
+
+    return (
+      <div>
+        <div style={{ border: '1px solid pink', margin: '10px' }}>
+          <div>Sub1: {this.state.myBar}</div>
+          <div>Sub1: {foo}</div>
+          <button onClick={this.setAnyThing}>setFoo</button>
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+if (import.meta.hot) {
+  import.meta.hot.accept()
+}
