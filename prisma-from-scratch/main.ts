ac484e7cb6a34e308dca4a28a32d2a6f0f2b3cbf
hisland <hisland@qq.com>
2022-03-08 17:23:58 +0800

    add prisma test

---
+++ prisma-from-scratch/main.ts
@@ -0,0 +1,24 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // ... you will write your Prisma Client queries here
+  await prisma.user.create({
+    data: {
+      name: 'Alice2',
+      email: 'alice2@prisma.io',
+    },
+  })
+
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
