03ca93f5649e35a52bf08aa10c403015a2e59a07
29027-何定良 <dingliang.he@hand-china.com>
2023-11-07 23:55:15 +0800

    add font-face-font-family test

---
+++ 02-vite4-vue3-ts/031.font-face-font-family/AppEntry.vue
@@ -0,0 +1,53 @@
+<script setup lang="tsx">
+import { ref, reactive, onMounted, computed } from 'vue'
+</script>
+<template>
+  <div style="border: 1px solid #eee; padding: 10px">
+    <div class="aa">abc 123</div>
+    <div class="bb">这是中文</div>
+    <div class="cc">abc 中英数字混合 123</div>
+    <div>
+      <span class="dd">abc 中英数字混合 123</span>
+    </div>
+    <div style="background: #1f1f1f">
+      <span class="ee">abc 中英数字混合 123</span>
+    </div>
+  </div>
+</template>
+<style scoped lang="less">
+.aa {
+  font-family: web1;
+}
+.bb {
+  font-family: local1;
+}
+.cc {
+  font-family: web1, serif, local1;
+}
+.dd {
+  background: linear-gradient(to right, red, blue);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+}
+.ee {
+  background: linear-gradient(to bottom, #fff, blue);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  font-style: italic;
+  font-weight: 700;
+}
+</style>
+
+<style lang="less">
+@font-face {
+  font-family: web1;
+  src: url('https://mdn.github.io/css-examples/web-fonts/VeraSeBd.ttf')
+    format('truetype');
+}
+@font-face {
+  font-family: local1;
+  src: url('./YouSheBiaoTiHei-2.ttf') format('truetype');
+}
+</style>
