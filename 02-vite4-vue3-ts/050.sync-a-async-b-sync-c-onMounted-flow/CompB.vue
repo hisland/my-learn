7991fa694617befd17af2e7623c9e2bbe91b9040
hisland <hisland@qq.com>
2024-04-01 17:51:50 +0800

    add vue3 test

---
+++ 02-vite4-vue3-ts/050.sync-a-async-b-sync-c-onMounted-flow/CompB.vue
@@ -0,0 +1,20 @@
+<script setup lang="tsx">
+import { defineAsyncComponent } from 'vue'
+import { onMounted, onUpdated } from 'vue'
+import CompC from './CompC.vue'
+
+console.log('CompB setup 1')
+
+onMounted(() => {
+  console.log('CompB onMounted')
+})
+
+console.log('CompB setup 2')
+</script>
+<template>
+  <div class="CompB" style="border: 1px solid pink; padding: 10px">
+    <div>CompB</div>
+    <CompC a1="a1"></CompC>
+  </div>
+</template>
+<style scoped></style>
---
+++ 02-vite4-vue3-ts/051.computed-if-false-branch-not-watched/CompA.vue
@@ -0,0 +1,28 @@
+<script setup lang="tsx">
+import { defineAsyncComponent } from 'vue'
+import { onMounted, onUpdated } from 'vue'
+import { ref, computed } from 'vue'
+
+const aa1 = ref(1)
+const aa2 = ref(1)
+const biggerThan10 = computed(() => {
+  return aa1.value > 10
+})
+const cc = computed(() => {
+  if (biggerThan10.value) {
+    return aa2.value
+  } else {
+    return 'less'
+  }
+})
+</script>
+<template>
+  <div class="CompA" style="border: 1px solid purple; padding: 10px">
+    <div>
+      <input type="text" v-model="aa2" />
+    </div>
+    <div>cc: {{ cc }}</div>
+    <div>只有 aa2 变化, cc 不会更新, 因为在 if false 里面才读取到</div>
+  </div>
+</template>
+<style scoped></style>
