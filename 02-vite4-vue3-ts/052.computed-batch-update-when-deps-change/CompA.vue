e1b496c9f9512bc7b7f67b8b6214a1ea2bc3cb84
hisland <hisland@qq.com>
2024-04-07 16:26:42 +0800

    update vue3 test

---
+++ 02-vite4-vue3-ts/052.computed-batch-update-when-deps-change/CompA.vue
@@ -0,0 +1,28 @@
+<script setup lang="tsx">
+import { defineAsyncComponent } from 'vue'
+import { onMounted, onUpdated } from 'vue'
+import { ref, computed } from 'vue'
+
+const aa1 = ref(2)
+const aa2 = ref(3)
+const sum = computed(() => {
+  console.log('sum: ')
+  return aa1.value + aa2.value
+})
+
+function setRandom() {
+  aa1.value = Math.random()
+  aa2.value = Math.random()
+}
+</script>
+<template>
+  <div class="CompA" style="border: 1px solid purple; padding: 10px">
+    <div>
+      <button @click="setRandom">同时设置2个值, computed 只计算一次</button>
+    </div>
+    <div>aa1: {{ aa1 }}</div>
+    <div>aa2: {{ aa2 }}</div>
+    <div>sum: {{ sum }}</div>
+  </div>
+</template>
+<style scoped></style>
---
+++ 02-vite4-vue3-ts/053.sync-set-get-right-now-and-multip-times-trigger-one-onUpdate/CompA.vue
@@ -0,0 +1,33 @@
+<script setup lang="tsx">
+import { defineAsyncComponent } from 'vue'
+import { onMounted, onUpdated } from 'vue'
+import { ref, computed } from 'vue'
+
+const aa1 = ref('abc')
+
+function setRandom() {
+  const rnd1 = Math.random().toString(36).substring(2, 8)
+  console.log('rnd1: ', rnd1)
+  aa1.value = rnd1
+  console.log('aa1.value: ', aa1.value)
+  const rnd2 = Math.random().toString(36).substring(2, 8)
+  console.log('rnd2: ', rnd2)
+  aa1.value = rnd2
+  console.log('aa1.value: ', aa1.value)
+}
+
+onUpdated(() => {
+  console.log('onUpdated aa1.value: ', aa1.value)
+})
+</script>
+<template>
+  <div class="CompA" style="border: 1px solid purple; padding: 10px">
+    <div>
+      <button @click="setRandom">
+        set同步get能取出来, 多次同步set, 最终只触发一次 onUpdated
+      </button>
+    </div>
+    <div>aa1: {{ aa1 }}</div>
+  </div>
+</template>
+<style scoped></style>
